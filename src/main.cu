b0d95ca78a24f199e3dc012248e3a43c17c2e49c
Derrekito <Derrekito@users.noreply.github.com>
2021-09-16 09:48:43 -0600

    WOOOOOOOOOOTIES!

--- src/main.cu
+++ src/main.cu
@@ -15,16 +15,16 @@
 using namespace std;
 
 __global__ void getback(uint8_t *device_in, uint8_t *device_out, int NUM_IMGS, int NUM_PIXELS) {  
-  /*
+  
   int idx = NUM_PIXELS*(blockIdx.x*blockDim.x + threadIdx.x);
-  if(idx < NUM_IMGS){
+  if(idx < NUM_IMGS * NUM_PIXELS){
     for(int i=0; i< NUM_PIXELS;i++){
       device_out[idx+i] = device_in[idx+i];
     }
-  }*/
-  for(int i=0;i<NUM_IMGS * NUM_PIXELS; i++){
-    device_out[i] = device_in[i];
   }
+  /*for(int i=0;i<NUM_IMGS * NUM_PIXELS; i++){
+    device_out[i] = device_in[i];
+  }*/
 }
 
 int main(int argc, char **argv){
@@ -32,10 +32,10 @@ int main(int argc, char **argv){
   string img_path = base_dir+"train-images-idx3-ubyte";
   string label_path = base_dir+"train-labels-idx1-ubyte";
 
-//  const int NUM_THREADS = 1024;
-//  const int NUM_BLOCKS = 59;
-  const int NUM_THREADS = 1;
-  const int NUM_BLOCKS = 1;
+  const int NUM_THREADS = 1024;
+  const int NUM_BLOCKS = 59;
+//  const int NUM_THREADS = 1;
+//  const int NUM_BLOCKS = 1;
 
   uint32_t *NUM_IMGS = (uint32_t*) malloc(sizeof(uint32_t));
   uint32_t *NUM_LABELS = (uint32_t*) malloc(sizeof(uint32_t));
@@ -80,7 +80,8 @@ int main(int argc, char **argv){
   string color;
   int bin_offset = 232; // greyscale color range is 232-255
   string print_exp;
-  for(int j=0;j<*NUM_IMGS * *NUM_ROWS * *NUM_COLS;j++){
+  for(int j=0;j<*NUM_IMGS * *NUM_PIXELS;j++){
+//  for(int j=0;j<100000;j++){
     if(j%28==0){
 	    if(j/28==0){printf("\n");}
 	      printf("\n");
